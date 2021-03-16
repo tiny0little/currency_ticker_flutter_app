d23851124cf904134f6aa37e3857db024af8a7e1
user <nromensky@gmail.com>
2021-03-16 04:51:39 +0300

    .

---
+++ android/app/src/main/kotlin/nickolay/romensky/currency_ticker_flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package nickolay.romensky.currency_ticker_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyCurrencyTickerApp()));
+}
+
+class MyCurrencyTickerApp extends StatefulWidget {
+  @override
+  _MyCurrencyTickerAppState createState() => _MyCurrencyTickerAppState();
+}
+
+class _MyCurrencyTickerAppState extends State<MyCurrencyTickerApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
